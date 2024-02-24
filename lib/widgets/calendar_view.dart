831b7df64bbf55eef8bf1ae01ec91164dfc46cfd
RacimFethallah <RacimFethallah@proton.me>
2024-02-24 23:33:02 +0100

    Mise à jour de la vue du calendrier avec affichage de la date et du jour de la semaine

--- lib/widgets/calendar_view.dart
+++ lib/widgets/calendar_view.dart
@@ -72,9 +72,46 @@ class _CalendarViewState extends State<CalendarView> {
             //   });
             // },
           ),
-          const SizedBox(height: 16),
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              today.year == DateTime.now().year &&
+                      today.month == DateTime.now().month &&
+                      today.day == DateTime.now().day
+                  ? "Today"
+                  : "${getWeekDay(today.weekday)} ${today.day}/${today.month}/${today.year} ",
+              textAlign: TextAlign.start,
+              style: const TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+
+
+  String getWeekDay(int weekday) {
+    switch (weekday) {
+      case 1:
+        return "Monday";
+      case 2:
+        return "Tuesday";
+      case 3:
+        return "Wednesday";
+      case 4:
+        return "Thursday";
+      case 5:
+        return "Friday";
+      case 6:
+        return "Saturday";
+      case 7:
+        return "Sunday";
+      default:
+        return "";
+    }
   }
 }
