cdec6d636c4fbe320bc362349e5ae547148788fe
RacimFethallah <RacimFethallah@proton.me>
2024-03-26 01:03:02 +0100

    Mise à jour des fonctionnalités de recherche***

--- lib/pages/list_page.dart
+++ lib/pages/list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:msh_checkbox/msh_checkbox.dart';
 import 'package:provider/provider.dart';
 import 'package:utask/providers/taskProvider.dart';
@@ -96,80 +97,117 @@ class _ListPageState extends State<ListPage> {
                   padding: const EdgeInsets.fromLTRB(24.0, 0, 24.0, 12),
                   child: Column(
                     children: [
-                      ExpansionTile(
-                        iconColor: Colors.white,
-                        collapsedIconColor: Colors.white,
-                        textColor: Colors.white,
-                        collapsedTextColor: Colors.white,
-                        collapsedBackgroundColor:
-                            const Color.fromARGB(255, 0, 73, 133),
-                        backgroundColor: const Color.fromARGB(255, 0, 73, 133),
-                        childrenPadding: const EdgeInsets.only(left: 20),
-                        collapsedShape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        initiallyExpanded: true,
-                        title: Row(
-                          children: [
-                            Text(
-                              taskLists[index].name,
-                              style: const TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(width: 20),
-                            Container(
-                              padding: const EdgeInsets.all(8),
-                              decoration: const BoxDecoration(
-                                color: Colors.white,
-                                // borderRadius: BorderRadius.circular(20),
-                                shape: BoxShape.circle,
-                              ),
-                              child: Text(
-                                taskLists[index].tasks.length.toString(),
+                      GestureDetector(
+                        onLongPress: () {
+                          // context
+                          //     .read<TasksProvider>()
+                          //     .deleteList(taskLists[index].id);
+                          // showDialog(
+                          //   context: context,
+                          //   builder: (context) {
+                          //     return AlertDialog(
+                          //       title: const Text('Delete List'),
+                          //       content: const Text(
+                          //           'Are you sure you want to delete this list?'),
+                          //       actions: [
+                          //         TextButton(
+                          //           onPressed: () {
+                          //             Navigator.pop(context);
+                          //           },
+                          //           child: const Text('Cancel'),
+                          //         ),
+                          //         TextButton(
+                          //           onPressed: () {
+                          //             // context
+                          //             //     .read<TasksProvider>()
+                          //             //     .deleteList(taskLists[index].id);
+                          //             Navigator.pop(context);
+                          //           },
+                          //           child: const Text('Delete'),
+                          //         ),
+                          //       ],
+                          //     );
+                          //   },
+                          // );
+                        },
+                        child: ExpansionTile(
+                          leading: const Icon(Icons.list),
+                          iconColor: Colors.white,
+                          collapsedIconColor: Colors.white,
+                          textColor: Colors.white,
+                          collapsedTextColor: Colors.white,
+                          collapsedBackgroundColor:
+                              const Color.fromARGB(255, 0, 73, 133),
+                          backgroundColor:
+                              const Color.fromARGB(255, 0, 73, 133),
+                          childrenPadding: const EdgeInsets.only(left: 20),
+                          collapsedShape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          initiallyExpanded: true,
+                          title: Row(
+                            children: [
+                              Text(
+                                taskLists[index].name,
                                 style: const TextStyle(
-                                    fontSize: 14, color: Colors.black),
+                                    fontSize: 16, fontWeight: FontWeight.bold),
                               ),
+                              const SizedBox(width: 20),
+                              Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  // borderRadius: BorderRadius.circular(20),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Text(
+                                  taskLists[index].tasks.length.toString(),
+                                  style: const TextStyle(
+                                      fontSize: 14, color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                          children: [
+                            ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: taskLists[index].tasks.length,
+                              itemBuilder: (context, taskIndex) {
+                                return ListTile(
+                                  leading: MSHCheckbox(
+                                    size: 22,
+                                    value: taskLists[index]
+                                        .tasks[taskIndex]
+                                        .isDone,
+                                    // colorConfig: MSHColorConfig
+                                    //     .fromCheckedUncheckedDisabled(
+                                    //   checkedColor:
+                                    //       const Color.fromARGB(255, 0, 73, 133),
+
+                                    // ),
+                                    style: MSHCheckboxStyle.fillScaleColor,
+                                    onChanged: (selected) {
+                                      context.read<TasksProvider>().updateTask(
+                                          taskLists[index].tasks[taskIndex].id,
+                                          selected);
+                                    },
+                                  ),
+                                  textColor: Colors.white,
+                                  title: Text(
+                                    taskLists[index].tasks[taskIndex].name,
+                                    style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                );
+                              },
                             ),
                           ],
                         ),
-                        children: [
-                          ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: taskLists[index].tasks.length,
-                            itemBuilder: (context, taskIndex) {
-                              return ListTile(
-                                leading: MSHCheckbox(
-                                  size: 22,
-                                  value:
-                                      taskLists[index].tasks[taskIndex].isDone,
-                                  // colorConfig: MSHColorConfig
-                                  //     .fromCheckedUncheckedDisabled(
-                                  //   checkedColor:
-                                  //       const Color.fromARGB(255, 0, 73, 133),
-
-                                  // ),
-                                  style: MSHCheckboxStyle.fillScaleColor,
-                                  onChanged: (selected) {
-                                    context.read<TasksProvider>().updateTask(
-                                        taskLists[index].tasks[taskIndex].id,
-                                        selected);
-                                  },
-                                ),
-                                textColor: Colors.white,
-                                title: Text(
-                                  taskLists[index].tasks[taskIndex].name,
-                                  style: const TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ],
                       ),
                       // const SizedBox(height: 10),
                     ],
