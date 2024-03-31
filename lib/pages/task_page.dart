7a59c05d02ff044f54f2040c8e48601071cb41e2
RacimFethallah <RacimFethallah@proton.me>
2024-03-31 02:13:21 +0100

    Ajouter la barre d'onglets des tâches

--- lib/pages/task_page.dart
+++ lib/pages/task_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../widgets/taskPageWidgets/horizontal_priority_view.dart';
 import '../widgets/inboxPage/tasks_view_inboxpage.dart';
+import '../widgets/taskPageWidgets/task_tab_bar.dart';
 import 'list_page.dart';
 
 class TasksPage extends StatefulWidget {
@@ -39,58 +40,14 @@ class _TasksPageState extends State<TasksPage>
           child: TabBarView(
             controller: _tabController,
             children: const [
-              // ListPage(),
               TasksViewInboxPage(),
               ListPage(),
             ],
           ),
         ),
-        // TasksViewInboxPage(),
       ],
     );
   }
 }
 
-class TaskTabBar extends StatelessWidget {
-  const TaskTabBar({
-    super.key,
-    required TabController tabController,
-  }) : _tabController = tabController;
 
-  final TabController _tabController;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.shade300,
-            spreadRadius: 1,
-            blurRadius: 10,
-            offset: const Offset(0, 10), // changes position of shadow
-          ),
-        ],
-        color: Colors.white,
-      ),
-      child: TabBar(
-        controller: _tabController,
-        indicatorSize: TabBarIndicatorSize.tab,
-        isScrollable: false,
-        tabs: const [
-          Tab(
-            iconMargin: EdgeInsets.only(bottom: 2),
-            text: 'Inbox',
-            icon: Icon(Icons.inbox_rounded),
-          ),
-          Tab(
-            text: 'Lists',
-            icon: Icon(Icons.format_list_bulleted_rounded),
-            iconMargin: EdgeInsets.only(bottom: 2),
-          )
-        ],
-      ),
-    );
-  }
-}
